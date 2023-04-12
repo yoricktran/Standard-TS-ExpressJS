39eedfb5c860c021813fb8a150bad89feb9fadb6
Yorick Tran <yoricktran@gmx.com>
2023-04-12 15:38:40 +0200

    Express + Typescript included

---
+++ index.ts
@@ -0,0 +1,31 @@
+/* const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+}); */
+
+import express, {Express, Request, Response} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express TypeScript Server');
+});
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
